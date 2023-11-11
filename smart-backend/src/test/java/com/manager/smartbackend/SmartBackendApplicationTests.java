e727d8a3feac3f31799e2f81c951cf8101057b03
InstitutoSerMelhor <devwebsermelhor@gmail.com>
2023-11-11 20:12:24 -0300

    setup: Add configuração base para iniciar o projeto.

---
+++ smart-backend/src/test/java/com/manager/smartbackend/SmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.smartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
